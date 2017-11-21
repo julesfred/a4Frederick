96d0a421e511bea7de1b760e06ca30ce81d8b758
julesfred <jules.tf4@yahoo.com>
2017-11-21 14:01:04 -0500

    restart button

--- storyComponent.js
+++ storyComponent.js
@@ -3,7 +3,7 @@
             controller: "GetController",
              template: ` 
             <p>One find day, my trusty {{$ctrl.addInfo.animal}} woke up to find a {{$ctrl.addInfo.color}} cat. The cat was {{$ctrl.addInfo.verb}} {{$ctrl.addInfo.adverb}}. What a weird morning...</p>
-            <button type="button">Restart Story</button>
+            <a href="#!/form"><button type="button">Restart Story</button></a>
         `
         };
 
